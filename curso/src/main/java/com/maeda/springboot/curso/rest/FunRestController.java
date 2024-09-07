f7eaf7b689ed40dec3c4f97c757530641e60e1bc
matheus <pedrosom482@gmail.com>
2024-09-07 13:27:49 -0300

    spring

--- curso/src/main/java/com/maeda/springboot/curso/rest/FunRestController.java
+++ curso/src/main/java/com/maeda/springboot/curso/rest/FunRestController.java
@@ -8,10 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     // expose "/" that returns hello world.
-
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5K";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "today is your lucky day";
     }
 
 }
